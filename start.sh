a2f4f28097f2d48baad7139a39189e8634de970c
Sebastian James <sebaxj@stanford.edu>
2023-05-17 23:37:33 -0700

    updated start script

--- start.sh
+++ start.sh
@@ -2,7 +2,6 @@
 
 # Run the command once to build the Docker image
 # Run again if Dockerfile is changed
-docker build -t cs247g-server .
+# docker build -t cs247g-server .
 
-docker rm cs247g-server
-docker run -it -v "$(pwd)"/server:/server -p 25565:25565 --name="cs247g-server" cs247g-server
+docker run -it -v "$(pwd)"/server:/server -p 25565:25565 cs247g-server
